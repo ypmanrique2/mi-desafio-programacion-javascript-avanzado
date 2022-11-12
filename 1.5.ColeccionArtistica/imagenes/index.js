c45600872db9754dc236a3b7b6cd5ff4fdc8be9c
ypmanrique2 <ypmanrique2@misena.edu.co>
2022-11-12 17:02:16 -0500

    1.5.ColeccionArtistica

---
+++ 1.5.ColeccionArtistica/imagenes/index.js
@@ -0,0 +1,38 @@
+const imagenes = [
+    {
+        id: 0,
+        src: 'imagenes/british-shorthair-cat-mieze-thoroughbred-preview (1).jpg'
+    },
+    {
+        id: 1,
+        src: 'imagenes/british-shorthair-cat-mieze-thoroughbred-preview (2).jpg'
+    },
+    {
+        id: 2,
+        src: 'imagenes/british-shorthair-cat-mieze-thoroughbred-preview (3).jpg'
+    },
+    {
+        id: 3,
+        src: 'imagenes/british-shorthair-cat-mieze-thoroughbred-preview.jpg'
+    },
+    {
+        id: 4,
+        src: 'imagenes/british-shorthair-cat-mieze-thoroughbred-preview.jpg'
+    },
+    {
+        id: 5,
+        src: 'imagenes/british-shorthair-rest-relaxed-meadow-preview.jpg'
+    },
+    {
+        id: 6,
+        src: 'imagenes/cat-british-shorthair-concerns-rest-preview.jpg'
+    },
+    {
+        id: 7,
+        src: 'imagenes/cat-british-shorthair-thoroughbred-fur-preview.jpg'
+    },
+    {
+        id: 8,
+        src: 'imagenes/domestic-cat-cat-british-shorthair-concerns-preview.jpg'
+    }
+]
