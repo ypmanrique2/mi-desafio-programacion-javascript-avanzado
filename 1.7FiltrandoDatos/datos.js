8bdd8cbf0cec03c24d830aad9d3a40070e54bfda
YADIN PAULO MANRIQUE MARQUEZ <ypmanrique2@misena.edu.co>
2022-11-17 19:21:00 -0500

    descarga de archivos

---
+++ 1.7FiltrandoDatos/datos.js
@@ -0,0 +1,74 @@
+const DATOS = [
+    {
+        "id": "623556c836",
+        "nombre": 'Greer Greene',
+        "compania": "Kongene",
+        "fdn": "1993-06-24",
+        "direccion": "17 Arion Place, Whitmer, Marshall Islands",
+        "acerca": "Ut"
+    },
+    {
+        "id": "5219829g5289g19mg5mgf",
+        "nombre": 'Donna Reese',
+        "compania": "Magnemo",
+        "fdn": "1991-02-28",
+        "direccion": "33 Ira Arion Place, Whitmer, Marshall Islands",
+        "acerca": "Alqila"
+    },
+    {
+        "id": "5f414851hj41d54j1gd1hj",
+        "nombre": 'Wells Phelps',
+        "compania": "Zialatic",
+        "fdn": "1993-01-13",
+        "direccion": "17 Division Place, Whitmer, Marshall Islands",
+        "acerca": "Incidunt"
+    },
+    {
+        "id": "515619561561956t1j5561651t",
+        "nombre": 'Muriel Doyle',
+        "compania": "Zorromop",
+        "fdn": "1991-11-01",
+        "direccion": "37 Arion Place, Whitmer, Marshall Islands",
+        "acerca": "Enim"
+    },
+    {
+        "id": "0623556c836",
+        "nombre": 'Greer Greene',
+        "compania": "Kongene",
+        "fdn": "1993-06-24",
+        "direccion": "17 Arion Place, Whitmer, Marshall Islands",
+        "acerca": "Ut"
+    },
+    {
+        "id": "0623556c836",
+        "nombre": 'Greer Greene',
+        "compania": "Kongene",
+        "fdn": "1993-06-24",
+        "direccion": "17 Arion Place, Whitmer, Marshall Islands",
+        "acerca": "Ut"
+    },
+    {
+        "id": "0623556c836",
+        "nombre": 'Greer Greene',
+        "compania": "Kongene",
+        "fdn": "1993-06-24",
+        "direccion": "17 Arion Place, Whitmer, Marshall Islands",
+        "acerca": "Ut"
+    },
+    {
+        "id": "0623556c836",
+        "nombre": 'Greer Greene',
+        "compania": "Kongene",
+        "fdn": "1993-06-24",
+        "direccion": "17 Arion Place, Whitmer, Marshall Islands",
+        "acerca": "Ut"
+    },
+    {
+        "id": "0623556c836",
+        "nombre": 'Greer Greene',
+        "compania": "Kongene",
+        "fdn": "1993-06-24",
+        "direccion": "17 Arion Place, Whitmer, Marshall Islands",
+        "acerca": "Ut"
+    }
+]
