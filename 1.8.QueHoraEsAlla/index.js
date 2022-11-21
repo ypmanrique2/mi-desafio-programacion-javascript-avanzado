e4e6b8f6e628ac520819ae4e09e4babae6cf65a0
YADIN PAULO MANRIQUE MARQUEZ <ypmanrique2@misena.edu.co>
2022-11-21 17:12:06 -0500

    1.9.CodigoMorse
    1.9.CodigoMorse

---
+++ 1.8.QueHoraEsAlla/index.js
@@ -0,0 +1,64 @@
+
+const zonasHorarias = [
+    {
+        "offset": -10,
+        "texto": "(UTC-10:00) Hawaii",
+    },
+    {
+        "offset": -6,
+        "texto": "(UTC-06:00) Central America",
+    },
+    {
+        "offset": -8,
+        "texto": "(UTC-09:00) Alaska",
+    },
+    {
+        "offset": -7,
+        "texto": "(UTC-07:00) Pacific DayLaigth Time (US & Canada)",
+    },
+    {
+        "offset": -7,
+        "texto": "(UTC-07:00) Arizona",
+    },
+    {
+        "offset": 1,
+        "texto": "(UTC-07:00) Dublin, Lisbon",
+    },
+    {
+        "offset": 0,
+        "texto": "(UTC) Greenwich Standard Time",
+    },
+    {
+        "offset": 2,
+        "texto": "(UTC+01:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna",
+    },
+    {
+        "offset": 4,
+        "texto": "(UTC+04:00) Abu Dhabi, Muscat",
+    },
+    {
+        "offset": 5.5,
+        "texto": "(UTC+05:30) Chennai, Kolkata, Numbai, New Delhi",
+    },
+    {
+        "offset": 7,
+        "texto": "(UTC+07:00) BangkoK, Hanoi, Jakarta",
+    },
+    {
+        "offset": 8,
+        "texto": "(UTC+08:00) Perth",
+    }
+];
+
+const ms_horas = 1000 * 60 * 60;
+
+for(const zona of zonasHorarias){
+    console.log(`${zona.texto}: ${calcularZonaHoraria(zona.offset)}`);
+}
+
+function calcularZonaHoraria(offset){
+    const hoy = new Date();
+    const utc = hoy.getTime();
+    const zonaTiempo = new Date(utc + (offset * ms_horas));
+    return zonaTiempo.toLocaleString();
+}
