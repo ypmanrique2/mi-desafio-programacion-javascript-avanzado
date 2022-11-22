6c6ae537f3d5ebc4f942c4726af7a35bd3b20b96
YADIN PAULO MANRIQUE MARQUEZ <ypmanrique2@misena.edu.co>
2022-11-22 13:10:15 -0500

    1.7.FiltrandoDatos

--- 1.7.FiltrandoDatos/js/index.js
+++ 1.7.FiltrandoDatos/js/index.js
@@ -1,6 +1,6 @@
 
-// Implementar un buscador de datos, donde los resultados se muestren tan 
-// pronto el usuario escriba el texto en el imput disponible
+// Implementar un buscador de datos, donde los resultados se muestren tan
+// pronto el usuario escriba texto en el input disponible
 
 const input = document.getElementById('input');
 const resultados = document.getElementById('resultados');
@@ -20,12 +20,11 @@ function generarElemento(info) {
 
     const span = document.createElement('span');
     span.innerHTML = `<b>id:</b> ${info.id}<br>
-        <b>nombre:</b> ${info.nombre}<br>
-        <b>compania</b> ${info.compania}<br>
-        <b>fecha de nacimiento:</b> ${info.fdn}<br>
-        <b>direccion:</b> ${info.direccion}<br>
-        <b>acerca:</b> ${info.acerca}`;
+    <b>nombre:</b> ${info.nombre}<br>
+    <b>compañia:</b> ${info.compania}<br>
+    <b>fecha de nacimiento:</b> ${info.fdn}<br>
+    <b>dirección:</b> ${info.direccion}<br>
+    <b>acerca:</b> ${info.acerca}`;
 
-        return span;
+    return span;
 }
-
