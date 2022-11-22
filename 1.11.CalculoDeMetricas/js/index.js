301719313d58a76b7badea8e28b4ccb6d15f78c0
YADIN PAULO MANRIQUE MARQUEZ <ypmanrique2@misena.edu.co>
2022-11-22 07:56:09 -0500

    1.11.CalculoDeMetricas

---
+++ 1.11.CalculoDeMetricas/js/index.js
@@ -0,0 +1,15 @@
+
+// 1. Cargar el archivo JSON metricas.js
+// 2. Mostrar tabla con el promedio de evaluación dividido por mes
+// 3. Mostrar tabla con el promedio de evaluación dividido por localidad
+
+const tablaLocalidad = document.getElementById('tabla-localidad');
+const tablaMes = document.getElementById('tabla-mes');
+
+
+
+function generarFila(texto, valor) {
+    const tr = document.createElement('tr');
+    const td1 = document.createElement('td');
+    
+}
